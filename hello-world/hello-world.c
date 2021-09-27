569d607b44d06aab29809251be89f0434f162b24
Ismael Amezcua <ismaelamezcua@ucol.mx>
2021-09-27 12:23:23 -0500

    First commit

---
+++ hello-world/hello-world.c
@@ -0,0 +1,17 @@
+#include "contiki.h"
+#include "dev/leds.h"
+
+#include <stdio.h>
+
+PROCESS(hello_world_process, "Hello World Process");
+AUTOSTART_PROCESSES(&hello_world_process);
+
+PROCESS_THREAD(hello_world_process, ev, data)
+{
+    PROCESS_BEGIN();
+
+    printf("Hello, World!\n");
+    leds_on(LEDS_ALL);
+
+    PROCESS_END();
+}
